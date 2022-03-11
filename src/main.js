4a0ccbd529e0f7326f5242ca1dbf5bc759fa9fe6
ali <2501.aliforward@gmail.com>
2022-03-11 21:59:37 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElIconModules from '@element-plus/icons-vue'
+// import Editor from '@tinymce/tinymce-vue'
+import mavonEditor from 'mavon-editor';
+import 'mavon-editor/dist/css/index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+// app.component('mavonEditor', mavonEditor)
+app.use(store).use(router).use(ElementPlus).use(mavonEditor).mount('#app')
+app.use(ElementPlus, { size: 'small', zIndex: 2000 })
+Object.keys(ElIconModules).forEach(function (key) {
+    app.component(ElIconModules[key].name, ElIconModules[key])
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import TodoListView from '../views/ToDoListView.vue'
+// import NoteView from '../views/NoteView.vue'
+// import TagSettingView from '../views/TagSettingView.vue'
+import AddToDo from '@/components/AddToDo.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: HomeView,
+    redirect: '/todo',
+    children: [
+      {
+        path: '/todo',
+        name: 'todo',
+        // 懒加载
+        // components: () => import(/* webpackChunkName: "about" */ '../views/ToDoListView.vue'),
+        component: TodoListView
+      },
+      {
+        path: '/note',
+        name: 'note',
+        // 懒加载
+        component: () => import(/* webpackChunkName: "about" */ '../views/NoteView.vue'),
+        // component: NoteView
+      },
+      {
+        path: '/tagSetting',
+        name: 'tagSetting',
+        // 懒加载
+        component: () => import(/* webpackChunkName: "about" */ '../views/TagSettingView.vue'),
+        // component: TagSettingView
+      }
+    ]
+  }, {
+    path: '/addToDo',
+    name: 'addToDo',
+    component: AddToDo
   }
 ]
 
