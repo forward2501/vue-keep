1b1e9513ca84170b781509b39a68e61a90f972af
ali <2501.aliforward@gmail.com>
2022-03-11 22:00:27 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 'off',
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'vue/multi-word-component-names': 0,
+    'eol-last': ["error", "never"],
+    'space-before-function-paren': 0,
+    "indent": "off",
   }
 }
