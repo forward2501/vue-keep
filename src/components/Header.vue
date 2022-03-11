1b2827e02f20bc0882ad9a343cc7d8dffec2a3c3
ali <2501.aliforward@gmail.com>
2022-03-11 21:57:49 +0800

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="header">
+    <el-tooltip v-model:visible="visible">
+      <template #content>
+        <span>主菜单</span>
+      </template>
+      <el-icon
+        class="menu-icon"
+        size="40px"
+        color="#5f6368"
+        @mouseenter="addMenuFocus($event)"
+        @mouseleave="removeMenuFocus($event)"
+        @click="openOrCloseMenu"
+        ><operation
+      /></el-icon>
+    </el-tooltip>
+    <h1 class="avatar">Keep</h1>
+    <el-input
+      v-model="inputSearch"
+      class="w-50 m-2 inputSearch"
+      placeholder="搜索"
+      icon="el-icon-search"
+      type="text"
+      clearable
+    >
+     <template #prepend><el-button><el-icon><search /></el-icon></el-button></template>
+    </el-input>
+    <el-divider class="el-divider"></el-divider>
+  </div>
+</template>
+
+<script>
+import store from "@/store/index.js";
+export default {
+  name: "Header",
+  data() {
+    return {
+      visible: false,
+      inputSearch: "",
+    };
+  },
+  store,
+  methods: {
+    addMenuFocus($event) {
+      $event.currentTarget.classList.add("focus");
+      this.visible = true;
+    },
+    removeMenuFocus($event) {
+      $event.currentTarget.classList.remove("focus");
+      this.visible = false;
+    },
+    // 菜单折叠事件
+    openOrCloseMenu() {
+      if (store.state.isCollapse === false) {
+        store.state.isCollapse = true;
+      } else {
+        store.state.isCollapse = false;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .menu-icon {
+    position: absolute;
+    top: 50%;
+    margin-top: -20px;
+  }
+  .avatar {
+    position: absolute;
+    top: 50%;
+    display: inline-block;
+    height: 40px;
+    line-height: 40px;
+    margin-top: -20px;
+    left: 70px;
+  }
+  .inputSearch {
+    position: absolute;
+    top: 50%;
+    margin-top: -25px;
+    width: 500px;
+    left: 300px;
+  }
+  .el-divider {
+    position: absolute;
+    bottom: 0;
+    margin:0
+  }
+}
+.focus {
+  cursor: pointer;
+  background-color: rgb(235, 231, 231);
+  border: 1px solid rgb(235, 231, 231);
+  border-radius: 50%;
+}
+// 深度选择器，修改el-input高度
+:deep(.el-input__inner) {
+  height: 50px;
+  background-color: #f9fcf8;
+}
+</style>
