c3a87bc8b77d974994b7666fac3b9cffb7ed2cd8
ali <2501.aliforward@gmail.com>
2022-03-11 21:55:05 +0800

    新增左侧菜单栏显示标签逻辑

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="el-drawer-div">
+    <el-menu
+      :default-active="currentMenuName"
+      active-text-color="blue"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      :router="true"
+      @select="select"
+    >
+      <el-menu-item index="todo">
+        <el-icon><list /></el-icon>
+        <template #title>记事</template>
+      </el-menu-item>
+       <el-menu-item index="notifyToDo">
+        <el-icon><bell /></el-icon>
+        <template #title>提醒</template>
+      </el-menu-item>
+      <el-menu-item index="note">
+        <el-icon><document /></el-icon>
+        <template #title>笔记</template>
+      </el-menu-item>
+      <div v-for="(item, index) in toDoTagsOptions" :key="index">
+        <el-menu-item :index="item.value">
+          <el-icon><price-tag /></el-icon>
+          <template #title>{{item.value}}</template>
+        </el-menu-item>
+      </div>
+       <el-menu-item index="tagSetting">
+        <el-icon><edit-pen /></el-icon>
+        <template #title>修改标签</template>
+      </el-menu-item>
+    </el-menu>
+    <!-- <el-drawer
+      v-model="isCollapse"
+      :with-header="false"
+      :modal="false"
+      direction="ltr"
+      size="20%"
+      :append-to-body="true"
+      :custom-class="drawer-custom"
+    >
+      <el-menu
+        :default-active="currentMenuName"
+        active-text-color="blue"
+        class="el-menu-vertical-demo"
+        :collapse="isCollapse"
+        :router="true"
+        @select="select"
+      >
+        <el-menu-item index="todo">
+          <el-icon><bell /></el-icon>
+          <template #title>记事</template>
+        </el-menu-item>
+        <el-menu-item index="note">
+          <el-icon><list /></el-icon>
+          <template #title>笔记</template>
+        </el-menu-item>
+        <el-menu-item index="tagSetting">
+          <el-icon><setting /></el-icon>
+          <template #title>标签管理</template>
+        </el-menu-item>
+      </el-menu>
+    </el-drawer> -->
+  </div>
+</template>
+<script>
+import store from "@/store/index.js";
+export default {
+  name: "LeftMenu",
+  data() {
+    return {};
+  },
+  store,
+  computed: {
+    isCollapse() {
+      return store.state.isCollapse;
+    },
+    currentMenuName() {
+      return store.state.currentMenu;
+    },
+    toDoTagsOptions() {
+      return store.state.toDoTagsOptions;
+    },
+  },
+  methods: {
+    // 菜单切换事件，selec是回调函数
+    select: function (index, indexPath, item, routeResult) {
+      console.log("当前选中菜单:" + index + ",路径:" + indexPath);
+      store.commit("updateCurrentMenu", index);
+      // 点击menu选项之后折叠menu
+      // store.commit("updateIsCollapse", false);
+      // console.log("折叠菜单");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+// .el-drawer,.rtl{
+//   margin-top: 114px;
+// }
+// :deep(.el-drawer__body){
+//   background-color: blue;
+// }
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 100%;
+  min-height: 800px;
+  margin-top: 5px;
+}
+.el-menu-item.is-active {
+  background-color: #42b893;
+  color: #fff;
+}
+</style>
