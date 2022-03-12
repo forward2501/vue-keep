045a77a3c391199a12d7ae6d5165dec04e45dd31
ali <2501.aliforward@gmail.com>
2022-03-12 22:09:43 +0800

    update

--- src/views/TagSettingView.vue
+++ src/views/TagSettingView.vue
@@ -1,14 +1,27 @@
 <template>
-<div>这是标签设置页</div>
+  <div>这是标签设置页</div>
+  <TagSetting :isShowTagSetting="isShowTagSetting"></TagSetting>
 </template>
 <script>
+import TagSetting from "@/components/TagSetting.vue";
+import store from "@/store/index.js";
 export default {
   name: "TagSettingView",
-  data () {
-    return {
-    };
-  }
-}
+  components: { TagSetting },
+  data() {
+    return {};
+  },
+  store,
+  computed: {
+    isShowTagSetting() {
+      if (store.state.currentMenu === "tagSetting") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
 </style>
